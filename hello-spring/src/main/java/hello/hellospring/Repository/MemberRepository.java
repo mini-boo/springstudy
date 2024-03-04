fe631ce10e643b4cc98041241d92aac7361e3e60
Hyunsoo Kim <sgn06221@gmail.com>
2024-03-04 21:42:33 +0900

    [feat] membership subproject

---
+++ hello-spring/src/main/java/hello/hellospring/Repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package hello.hellospring.Repository;
+
+import hello.hellospring.Domain.Member;
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
---
+++ hello-spring/src/main/java/hello/hellospring/Service/MemberService.java
@@ -0,0 +1,46 @@
+package hello.hellospring.Service;
+
+import hello.hellospring.Domain.Member;
+import hello.hellospring.Repository.MemberRepository;
+import hello.hellospring.Repository.MemoryMemberRepository;
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /*
+     * 회원 가입
+     * */
+    public long join(Member member){
+
+        //같은 이름이 있는 중복회원은 안 된다.
+        validateDuplicateMember(member);
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+
+    /*
+     * 전체 회원 조회
+     * */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
