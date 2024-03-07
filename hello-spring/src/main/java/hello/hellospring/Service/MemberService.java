fd029032ce3b8e77a47b35ed2abd7d86de7d13c4
Hyunsoo Kim <sgn06221@gmail.com>
2024-03-07 23:37:06 +0900

    [refactor]  Sevice Bean을 통해 의존관계 추가

--- hello-spring/src/main/java/hello/hellospring/Service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/Service/MemberService.java
@@ -6,7 +6,9 @@ import hello.hellospring.Repository.MemoryMemberRepository;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
